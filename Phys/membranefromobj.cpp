eb79da2abbdd38dad07dbd8a47aa4131e7f8a106
Mousavi <sayediman.mousavi@yale.edu>
2021-12-31 19:59:42 -0500

    Switch

--- Phys/membranefromobj.cpp
+++ Phys/membranefromobj.cpp
@@ -1,24 +1,30 @@
 #include "membranefromobj.h"
 #define _P 1
-#define _K 60
+#define _K 50
 #define _kappa 18522
 #define _T 0
 #define _E 2*_K/1.73205081
 #define _THRESHOLD 42
-#define _F 2001
+#define _F 2007
 #define _DT 1e-6
 
 Physics::MembraneFromObj::MembraneFromObj(double dt):SurfaceWithPhysics(),_dt(dt),_appliedF(_F),_py(500),_frad(55)
 {
     //Amixed
     _FSign=-1;
-    _radialForce=25*(_F%10);
+    int res=_F%10;
+    if(res>5){
+        _radialForce=25*(5-res);
+    }else{
+        _radialForce=25*res;
+    }
+
     _Rind=4;
     _cb=nullptr;
     _kappaFactor=1;
     _radiusFactor=1;
-    //_disc=new Geometry::WaveFrontObj(QString(R"(C:\Users\sm2983\Documents\Projects\Membrane\disc_r44_d50_relaxed.obj)"));
-    _disc=new Geometry::WaveFrontObj(QString(R"(C:\Users\sm2983\Documents\Projects\Membrane\disc_r44_d60_relaxed.obj)"));
+    _disc=new Geometry::WaveFrontObj(QString(R"(C:\Users\sm2983\Documents\Projects\Membrane\disc_r44_d50_relaxed.obj)"));
+    //_disc=new Geometry::WaveFrontObj(QString(R"(C:\Users\sm2983\Documents\Projects\Membrane\disc_r44_d60_relaxed.obj)"));
     //_disc=new Geometry::WaveFrontObj(QString(R"(C:\Users\sm2983\Documents\Projects\Membrane\oval3_r1_3scaled_smaller.obj)"));
     _tris=_disc->_tris;
     _scale=1e-5;
