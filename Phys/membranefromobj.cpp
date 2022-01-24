ba240bc232587cbb37708c79b2f7b8b80866cfe5
Mousavi <sayediman.mousavi@yale.edu>
2022-01-24 17:56:35 -0500

    Switch

--- Phys/membranefromobj.cpp
+++ Phys/membranefromobj.cpp
@@ -5,7 +5,7 @@
 #define _T 0
 #define _E 2*_K/1.73205081
 #define _THRESHOLD 42
-#define _F 2006
+#define _F 2004
 Physics::MembraneFromObj::MembraneFromObj(double dt):SurfaceWithPhysics(),_dt(dt),_appliedF(_F),_py(500),_frad(55),_alpha(0)
 {
     std::cout<<"Load From Files"<<std::endl;
